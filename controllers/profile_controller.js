e251876f56c4030cf1b397b575f70f167a623c04
TevennLewis <tevennlewis@gmail.com>
2021-09-15 12:20:17 -0700

    can view workouts of specific user, prepping for update route

--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -9,30 +9,9 @@ router.get('/:id', async (req, res, next) => {
     try {
         const foundUser = await User.findById(req.params.id);
         const allWorkout = await Workout.find({ user: req.params.id })
-            .populate({
-                path: 'exerciseOne',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseTwo',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseThree',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseFour',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseFive',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseSix',
-                model: Exercise
-            })
+            .populate(
+                'exerciseOne exerciseTwo exerciseThree exerciseFour exerciseFive exerciseSix'
+            )
             ;
         const context = {
             user: foundUser,
@@ -48,48 +27,69 @@ router.get('/:id', async (req, res, next) => {
     };
 });
 
-router.get('/:id/editworkout', async (req, res, next) => {
+// router.get('/:id/editworkout', async (req, res, next) => {
+//     try {
+//         const foundUser = await User.findById(req.params.id);
+//         const allWorkout = await Workout.find({ user: req.params.id })
+//             .populate({
+//                 path: 'exerciseOne',
+//                 model: Exercise
+//             })
+//             .populate({
+//                 path: 'exerciseTwo',
+//                 model: Exercise
+//             })
+//             .populate({
+//                 path: 'exerciseThree',
+//                 model: Exercise
+//             })
+//             .populate({
+//                 path: 'exerciseFour',
+//                 model: Exercise
+//             })
+//             .populate({
+//                 path: 'exerciseFive',
+//                 model: Exercise
+//             })
+//             .populate({
+//                 path: 'exerciseSix',
+//                 model: Exercise
+//             })
+//             ;
+//         const context = {
+//             user: foundUser,
+//             workouts: allWorkout,
+//         };
+//         console.log(context)
+//         return res.render('edit', context);
+
+//     } catch (error) {
+//         console.log(error);
+//         req.error = error;
+//         return next();
+//     };
+// });
+
+router.get('/:id/workout/:wid', async (req, res, next) => {
     try {
+        const foundWorkout = await Workout.findById(req.params.wid)
+        .populate('exerciseOne exerciseTwo exerciseThree exerciseFour exerciseFive exerciseSix');
         const foundUser = await User.findById(req.params.id);
-        const allWorkout = await Workout.find({ user: req.params.id })
-            .populate({
-                path: 'exerciseOne',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseTwo',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseThree',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseFour',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseFive',
-                model: Exercise
-            })
-            .populate({
-                path: 'exerciseSix',
-                model: Exercise
-            })
-            ;
+        
+        console.log(foundWorkout, foundUser);
+
         const context = {
+            workout: foundWorkout,
             user: foundUser,
-            workouts: allWorkout,
-        };
-        console.log(context)
-        return res.render('edit', context);
+        }
+    return res.render("edit", context);
 
     } catch (error) {
         console.log(error);
         req.error = error;
         return next();
-    };
-});
+    }
+})
 
 router.put('/:id', async (req, res, next) => {
     try {
@@ -98,13 +98,13 @@ router.put('/:id', async (req, res, next) => {
             { new: true });
 
         const foundUser = await User.findById(req.params.id);
-
+            console.log(foundUser);
         const context = {
             workout: updatedWorkout,
             user: foundUser,
         };
 
-        return res.redirect(`/user/${user.id}`);
+        return res.redirect(`/user/${foundUser._id}`);
 
     } catch (error) {
         console.log(error);
